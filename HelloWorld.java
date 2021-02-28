b4e02a78630b846b6dba55b3e3e7e3851307a0d1
yldas <misael.valentin@upr.edu>
2021-02-28 00:46:23 -0500

    Changed message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! This is my first Jenkins build on commit!");
+        System.out.println("This is my first Jenkins build on commit!");
     }
 }
